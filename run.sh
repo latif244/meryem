b2fca3486d144b60abe4194545dc377f29edf928
root <root@virthost01.skyline.lan>
2023-02-16 15:50:21 +0100

    first commit

---
+++ run.sh
@@ -0,0 +1,6 @@
+docker build -t meryem . 
+#docker run -it -d -p 8090:80 -v /home/ibrahim/sandbox/meryem/logs:/var/log/nginx:rw --name meryem meryem 
+docker run -it -d -p 8090:80 --restart always -v /home/ibrahim/sandbox/meryem/website:/www -v /home/ibrahim/sandbox/meryem/logs:/var/log/nginx:rw --name meryem meryem 
+#docker run -it meryem 
+#docker run -it node
+#docker exec -it meryem sh 
