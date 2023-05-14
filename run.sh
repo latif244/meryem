3c3a885133db9d09bcca8a6fa33301f4be1d482b
ibrahim <ibrahim@virthost01.skyline.lan>
2023-05-14 13:09:33 +0200

    Sun May 14 13:09:33 CEST 2023

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+docker rm -f meryem
 docker build -t meryem:latest -t meryem:v2.0 . 
 #docker run -it -d -p 8090:80 -v /home/ibrahim/sandbox/meryem/logs:/var/log/nginx:rw --name meryem meryem 
 docker run -it -d -p 8090:80 --name meryem --restart always -v /home/ibrahim/sandbox/meryem/website:/www -v /home/ibrahim/sandbox/meryem/logs:/var/log/nginx:rw meryem:latest 
