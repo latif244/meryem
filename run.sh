42c8a8b336f6e31aa4bb7b25ef7458288dae85cb
ibrahim <ibrahim@virthost01.skyline.lan>
2023-05-15 18:05:22 +0200

    Mon May 15 18:05:22 CEST 2023

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 docker rm -f meryem
+
+git add .
+git commit -am "$(date)"
+git push
+
 docker build -t meryem:latest -t meryem:v2.0 . 
 #docker run -it -d -p 8090:80 -v /home/ibrahim/sandbox/meryem/logs:/var/log/nginx:rw --name meryem meryem 
 docker run -it -d -p 8090:80 --name meryem --restart always -v /home/ibrahim/sandbox/meryem/website:/www -v /home/ibrahim/sandbox/meryem/logs:/var/log/nginx:rw meryem:latest 
 #docker run -it meryem 
 #docker run -it node
 #docker exec -it meryem sh
-git add .
-git commit -am "$(date)"
-git push
