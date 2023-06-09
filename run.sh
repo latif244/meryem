85b8b05de9f52c316cc702c4d211d59d6aa0382f
ibrahim <ibrahim@virthost01.skyline.lan>
2023-06-09 21:04:24 +0200

    Fri Jun  9 21:04:24 CEST 2023

--- run.sh
+++ run.sh
@@ -19,7 +19,6 @@ docker run -it -d \
     -p 8090:8000 \
     --name meryem \
     --restart always \
-    -v ./docker/code/db.sqlite3:/code/db.sqlite3:rw \
     meryem:latest
 
 #     -v ./docker/code:/code/db \
