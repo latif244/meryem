b33b48499e58893f176d05d108dfbc02bbe1d057
ibrahim <ibrahim@virthost01.skyline.lan>
2023-06-10 23:49:51 +0200

    Sat Jun 10 23:49:51 CEST 2023

--- website/views.py
+++ website/views.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail
 from django.views.decorators.csrf import csrf_protect
 from django.views.generic import TemplateView
 from django.shortcuts import render, HttpResponse
@@ -13,13 +14,23 @@ class FormMixin:
         if details.is_valid():
             post = details.save(commit=False)
             post.save()
+            email = details.cleaned_data.get('email')
+            subject = details.cleaned_data.get('subject')
+            message = details.cleaned_data.get('message')
+            send_mail(
+                [subject],
+                [message],
+                [email],
+                'info@skylinewebservice.de', # Replace with your email address
+                fail_silently=False,
+            )
             result = "Thank you"
             return HttpResponse(result, status=200)
         else:
             result = "Something went wrong. Please try again."
             return HttpResponse(result, status=400)
 
-# @csrf_protect
+
 class HomeView(FormMixin, TemplateView):
     template_name = "index.html"
 
