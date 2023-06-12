99212840babd529250a6205303b97ef3713e7fb7
ibrahim <ibrahim@virthost01.skyline.lan>
2023-06-12 07:40:25 +0200

    Mon Jun 12 07:40:25 CEST 2023

--- config/settings.py
+++ config/settings.py
@@ -141,9 +141,9 @@ EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' # Choose an email backend
 EMAIL_HOST = 'localhost' # Replace with your SMTP server hostname
-EMAIL_PORT = 8025 # Replace with your SMTP server port 587
+EMAIL_PORT = 587 # Replace with your SMTP server port 587
 EMAIL_USE_TLS = False # Replace with True if using TLS
-EMAIL_HOST_USER = 'info@embik.com' # Replace with your SMTP username
+EMAIL_HOST_USER = 'info@katja-meryem-bruegel.de' # Replace with your SMTP username
 EMAIL_HOST_PASSWORD = 'your_password' # Replace with your SMTP password
 
 PASSWORD_HASHERS = [
