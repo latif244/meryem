808ec3b3654f0c0b870a63d6b88a01980c04510a
IDDRISU LATIF <62152076+latif244@users.noreply.github.com>
2024-01-16 16:39:09 +0000

    Tue Jan 16 16:39:09 UTC 2024

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["katja-meryem-bruegel.de"]
 CSRF_TRUSTED_ORIGINS = [
     'https://meryerm.sawadogo.xyz',
     'https://*.sawadogo.xyz',
